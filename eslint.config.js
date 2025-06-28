3f943757524f7f71bc0ca85653f9927eb359a90f
P.Thamilselven <contact@tselven.com>
2025-06-28 12:03:02 +0530

    Add ESLint Support

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module", // or "commonjs" if using require()
+      globals: {
+        ...globals.browser,
+        ...globals.node, // ✅ adds 'process', '__dirname', etc.
+      },
+    },
+  },
+  {
+    ignores: [
+      "node_modules/",
+      "dist/",
+      "coverage/",
+      "*.config.js", // optional
+    ],
+  },
+]);
